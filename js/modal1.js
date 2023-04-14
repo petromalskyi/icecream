bd74ebc3981f452e1e07d8a040dc7f00085e45e9
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2023-04-14 15:21:38 +0300

    modal products

--- js/modal1.js
+++ js/modal1.js
@@ -7,20 +7,45 @@
     closeModalBtn: document.querySelector('[data-modal-close1]'),
     modal: document.querySelector('[data-modal1]'),
     changeNameEl: document.querySelector('[data-change-name]'),
+    changeImgEl: document.querySelector('[data-change-img]'),
     backdropEl: document.querySelector('.backdrop1'),
   };
- 
+
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn.addEventListener('click', changeName);
+  refs.openModalBtn.addEventListener('click', changeImg);
 
   refs.openModalBtn1.addEventListener('click', toggleModal);
   refs.openModalBtn1.addEventListener('click', changeName);
+  refs.openModalBtn1.addEventListener('click', changeImg2);
 
   refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener('click', changeName);
+  refs.openModalBtn2.addEventListener('click', changeImg3);
 
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.backdropEl.addEventListener('click', onBackdropClick);
+
+  function changeImg() {
+    refs.changeImgEl.classList.remove('modal1__img--img1');
+    refs.changeImgEl.classList.remove('modal1__img--img2');
+    refs.changeImgEl.classList.remove('modal1__img--img3');
+    refs.changeImgEl.classList.add('modal1__img--img1');
+  }
+
+  function changeImg2() {
+    refs.changeImgEl.classList.remove('modal1__img--img1');
+    refs.changeImgEl.classList.remove('modal1__img--img2');
+    refs.changeImgEl.classList.remove('modal1__img--img3');
+    refs.changeImgEl.classList.add('modal1__img--img2');
+  }
+
+  function changeImg3() {
+    refs.changeImgEl.classList.remove('modal1__img--img1');
+    refs.changeImgEl.classList.remove('modal1__img--img2');
+    refs.changeImgEl.classList.remove('modal1__img--img3');
+    refs.changeImgEl.classList.add('modal1__img--img3');
+  }
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden1');
